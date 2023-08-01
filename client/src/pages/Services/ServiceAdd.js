18920bd659276e8bef1cafcf11e790240c2e4443
zubeyircaymaz <zubeyir.caymaz@idvlabs.com>
2023-08-01 20:48:43 +0300

    bug fixed.

--- client/src/pages/Services/ServiceAdd.js
+++ client/src/pages/Services/ServiceAdd.js
@@ -4,6 +4,8 @@ import { FormInput, MultilineFormInput, ServiceTypeInput } from "../../component
 import axios from "axios";
 
 const ServiceAdd = () => {
+  const isLogged = localStorage.getItem('name') ? true : false
+
   const [serviceName, setserviceName] = useState("");
   const [serviceGsmno, setserviceGsmno] = useState("");
   const [serviceAddress, setserviceAddress] = useState("");
@@ -41,9 +43,10 @@ const ServiceAdd = () => {
       alert(err.response.data.msg);
     }
   };
-  return (
-    <>
-      <Grid container direction="row" p={3}>
+  const LoggedRouter = () => {
+    return (
+        <>
+<Grid container direction="row" p={3}>
         <Grid container direction="row" justifyContent="center" pb={2}>
           <Typography pt={2} pb={1} sx={{ fontSize: "32px", color: "#0f0f0f" }}>
             Servis Ekle
@@ -137,6 +140,26 @@ const ServiceAdd = () => {
           </form>
         </Grid>
       </Grid>
+        </>
+    )
+  }
+  const nonLoggedRouter = () => {
+    return (
+        <>
+            <Grid container alignItems={'center'} sx={{ height: '70vh' }}>
+                <Grid xs={12}>
+                    <Typography variant='h2' className='animate-charcter'
+                    sx={{ textAlign: 'center', width:'100%', height:'100%', fontWeight:'700', textTransform:'capitalize' }}>
+                        Giriş Yapınız...
+                    </Typography>
+                </Grid>
+            </Grid>
+        </>
+    )
+  }
+  return (
+    <>
+      {isLogged ? LoggedRouter() : nonLoggedRouter()}
     </>
   );
 };
